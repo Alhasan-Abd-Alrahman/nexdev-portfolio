ea7394332da251946ec5a6a3d65366cbe7394287
alhasan abd alrahamn <alhasan.abd.alrahman@>
2026-09-20 10:36:01 +0300

    fix home route2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <Route path="/team" element={<Team />} />
 
         <Route path="/contact" element={<Contact />} />
-        
+
           <Route path="*" element={<Home />} />
 
       </Routes>
